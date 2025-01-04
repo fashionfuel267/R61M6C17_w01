77c3ca46a35d82610a8697eb989ac6d220e8ca27
Shahanaj <fashionfuel267@gmail.com>
2025-01-04 13:03:12 +0600

    API_ConsumeAPI

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
-using System.Web.Mvc;
+//using System.Web.Mvc;
 using R61M6C17_w01.Models;
 
 
 namespace R61M6C17_w01.Controllers
 {
     [EnableCors("*", "*", "*")]
+    [RoutePrefix("api/Categories")]
     public class CategoriesController : ApiController
     {
         private readonly InventoryContext db=new InventoryContext();
@@ -21,10 +23,17 @@ namespace R61M6C17_w01.Controllers
         {
             return db.Categories;
         }
-        [System.Web.Http.Route("ParentCategory")]
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("GetParentCategory")]
         public IEnumerable<Category> GetParentCategory()
         {
             return db.Categories.Where(c=>c.ParentID==0);
+        }
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("ChildCategory")]
+        public IEnumerable<Category> ChildCategory(int PId)
+        {
+            return db.Categories.Where(c => c.ParentID == PId);
         }
         public Category Get(int Id)
         {
@@ -37,6 +46,7 @@ namespace R61M6C17_w01.Controllers
         public void Put(Category category)
         {
             db.Entry(category).State= System.Data.Entity.EntityState.Modified;
+           
             db.SaveChanges();
         }
         public void Delete(int Id)
--- Models/InventoryContext.cs
+++ Models/InventoryContext.cs
@@ -14,8 +14,10 @@ namespace R61M6C17_w01.Models
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Purchase> Purchases { get; set; }
-        public DbSet<Sales> Sales { get; set; }
+        public DbSet<SalesOrder> SalesOrder { get; set; }
         public DbSet<Stock> Stocks { get; set; }
+        public DbSet<PurchaseDetail> PurchaseDetails { get; set; }
+        public DbSet<SalesOrderDetail> SalesOrderDetails { get; set; }
     }
     public class Category
     {
@@ -44,24 +46,54 @@ namespace R61M6C17_w01.Models
         [DataType(DataType.Date),DisplayFormat(DataFormatString ="{0:dd-MM-yy}",
             ApplyFormatInEditMode =true)]
         public DateTime PurchaseDate { get; set; }
-        [ForeignKey("Product")]
-        public int ProductId { get; set; }
-        public int Qty { get; set; }
+         
+        public string PurchaseNumber { get; set; }
         public string VendorName { get; set; }
-        public virtual Product Product { get; set; }
+        public double Total { get; set; }
+    public ICollection<PurchaseDetail> Details { get; set; }
     }
-    public class Sales
+    public class PurchaseDetail
+    {
+        [Key]
+        public int ID { get; set; }
+        [ForeignKey(nameof(Purchase))]
+        public int PurchaseID { get; set; }
+        public string ItemName { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Discount { get; set; }
+        public decimal Total { get; set; }
+        public virtual Purchase Purchase { get; set; }
+    }
+
+    public class SalesOrder
     {
         public int Id { get; set; }
         [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:dd-MM-yy}", 
             ApplyFormatInEditMode = true)]
         public DateTime SalesDate { get; set; }
+         
+         public string  OrderNumber { get; set; }
+        public string CustomerName { get; set; }
+        public double TotalPrice { get; set; }
+
+        //public virtual Product Product { get; set; }
+        public ICollection<SalesOrderDetail> SalesOrderDetails { get; set; }
+    }
+    public class SalesOrderDetail
+    {
+        [Key]
+        public int ID { get; set; }
+        [ForeignKey(nameof(SalesOrder))]
+        public int OrderId { get; set; }
         [ForeignKey("Product")]
         public int ProductId { get; set; }
-        public int Qty { get; set; }
-        public string CustomerName { get; set; }
-
-        public virtual Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Discount { get; set; }
+        public decimal Total { get; set; }
+        public Product Product { get; set; }
+        public virtual SalesOrder SalesOrder { get; set; }
     }
 
     public class Stock
